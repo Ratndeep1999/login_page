92e736dfbe6ca7cf387497882e14184a7511e8f0
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-22 20:46:12 +0530

    login_page.dart :

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,6 +10,15 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
+            child: Form(child: Column(
+              children: [
+                TextFormField(),
+              ],
+            ),),
+        ),
+    );
   }
 }
