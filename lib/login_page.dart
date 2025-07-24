c70b48dc4d1096bc6be1c8928ad083b525c20452
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-25 01:55:38 +0530

    login_page.dart : 1) _name input field and label add, form key add, validation add to name field, text button add, validation method add in Text Button 2) _email input field and label add, validation also add, 3) _password input field and label add, hint text add, validation add for a-z, A-Z, 0-9, !@#\$*~_, suffix icon add with validation, value store in another variable, all validations completely added 4) conform password field add, visibility logic added to conform password, re triggering logic add using onChanged method 5) Mobile number input field add, controller add, validation add, max length add, icon add 6) Address input field add, controller add, validation add, 7) Gender radio buttons add using RadioMenuButton() for male (icon), female and other

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -442,7 +442,7 @@ class _LoginPageState extends State<LoginPage> {
                                 // _passwordController.text = "Rajratna@121" ;
                                 // _addressController.text = "Bhim wadi, thool layout, sindhi meghe, wardha, 442001" ;
 
-
+                                // show snackBar if condition meet
                                 if ( _gender.isEmpty || _gender == '' || _gender == 'Not selected' ) {
                                   ScaffoldMessenger.of(context).showSnackBar(
                                       SnackBar(
